95db586bebbd8e66937135a2b44026d9b5cba9cc
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-08-14 13:19:38 +0000

    Updating to latest FlutterFlow output.

---
+++ android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.activetraveltours.gauth
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/index.dart
@@ -0,0 +1,3 @@
+// Export pages
+export '/authenticate/authenticate_widget.dart' show AuthenticateWidget;
+export '/logged_in/logged_in_widget.dart' show LoggedInWidget;
